110eb8a22ec2ea60481ee9b2fd54051099dad684
isa101101 <isabellefredriksson@gmail.com>
2022-01-01 22:07:01 +0100

    Update Områdeschefer.java

--- src/mib/Områdeschefer.java
+++ src/mib/Områdeschefer.java
@@ -4,16 +4,21 @@
  */
 package mib;
 
+import oru.inf.InfDB;
+import oru.inf.InfException;
+
 /**
  *
  * @author isabellefredriksson
  */
 public class Områdeschefer extends javax.swing.JFrame {
 
+    private InfDB idb;
     /**
      * Creates new form Områdeschefer
      */
-    public Områdeschefer() {
+    public Områdeschefer(InfDB idb) {
+        this.idb = idb;
         initComponents();
     }
 
@@ -130,7 +135,7 @@ public class Områdeschefer extends javax.swing.JFrame {
         /* Create and display the form */
         java.awt.EventQueue.invokeLater(new Runnable() {
             public void run() {
-                new Områdeschefer().setVisible(true);
+              
             }
         });
     }
