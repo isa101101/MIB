57705b60f4de866106214b19c591d4385fb2ba8b
isa101101 <isabellefredriksson@gmail.com>
2022-01-07 10:54:02 +0100

    123

--- src/mib/ÄndraAgentLösenord.java
+++ src/mib/ÄndraAgentLösenord.java
@@ -119,7 +119,7 @@ public class ÄndraAgentLösenord extends javax.swing.JFrame {
                     .addComponent(txtBefintligtLösenord, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE)
                     .addComponent(btnHämta))
                 .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.UNRELATED)
-                .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.BASELINE)
+                .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
                     .addComponent(lblNyttLösenord)
                     .addComponent(txtNyttLösenord, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE))
                 .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED)
